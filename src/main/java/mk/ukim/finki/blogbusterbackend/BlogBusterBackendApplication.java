4e589f676aa505a583facee685302fd128465348
Boris Stojchevski <stojcevskib.02@gmail.com>
2024-03-28 21:00:05 +0100

    Init project config, add application profiles

---
+++ src/main/java/mk/ukim/finki/blogbusterbackend/BlogBusterBackendApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.blogbusterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBusterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogBusterBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/blogbusterbackend/BlogBusterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.blogbusterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBusterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
