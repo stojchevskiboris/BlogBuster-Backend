b21a7668234534aa3ec391ac99a4785d9dbdfaf4
katerinapoposka <katevpoposka@gmail.com>
2024-05-12 23:29:31 +0200

    Created discoverPeople & discoverCategories methods

--- src/main/java/mk/ukim/finki/blogbusterbackend/service/UserService.java
+++ src/main/java/mk/ukim/finki/blogbusterbackend/service/UserService.java
@@ -1,10 +1,13 @@
 package mk.ukim.finki.blogbusterbackend.service;
 
+import mk.ukim.finki.blogbusterbackend.model.User;
+import mk.ukim.finki.blogbusterbackend.model.dto.UserDTO;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.List;
 
 
 public interface UserService {
     UserDetailsService userDetailsService();
-
+    List<UserDTO> discoverPeople(Long userId);
 }
--- src/main/java/mk/ukim/finki/blogbusterbackend/service/impl/UserService.java
+++ src/main/java/mk/ukim/finki/blogbusterbackend/service/impl/UserService.java
@@ -2,6 +2,9 @@ package mk.ukim.finki.blogbusterbackend.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import mk.ukim.finki.blogbusterbackend.model.User;
+import mk.ukim.finki.blogbusterbackend.model.dto.UserDTO;
+import mk.ukim.finki.blogbusterbackend.model.exceptions.InvalidUserIdException;
+import mk.ukim.finki.blogbusterbackend.model.mappers.UserMapper;
 import mk.ukim.finki.blogbusterbackend.repository.UserRepository;
 
 
@@ -11,7 +14,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -31,6 +36,15 @@ public class UserService implements mk.ukim.finki.blogbusterbackend.service.User
         };
 
     }
-
+    @Override
+    public List<UserDTO> discoverPeople(Long userId) {
+        User currentUser = userRepository.findById(userId).orElseThrow(InvalidUserIdException::new);
+        List<User> nonFollowers = userRepository.findAll().stream().
+                filter(user -> !user.equals(currentUser))
+                .filter(user -> !user.getFollowingUsers().contains(currentUser))
+                .limit(10)
+                .collect(Collectors.toList());
+        return UserMapper.MapToListViewModel(nonFollowers);
+    }
 
 }
