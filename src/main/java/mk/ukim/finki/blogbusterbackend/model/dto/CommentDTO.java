59265a4c4318c624175bcaabe9059c260ff5de69
Stefan Stosevski <sfnn.stoshevski@gmail.com>
2024-05-18 22:42:16 +0200

    Last push for localhost

--- src/main/java/mk/ukim/finki/blogbusterbackend/model/dto/CommentDTO.java
+++ src/main/java/mk/ukim/finki/blogbusterbackend/model/dto/CommentDTO.java
@@ -13,7 +13,7 @@ public class CommentDTO {
     private Long postId;
     private LocalDateTime comment_date;
 
-    public CommentDTO(Long id, @NonNull String content, @NonNull String authorUsername, @NonNull Long postId, LocalDateTime comment_date) {
+    public CommentDTO(Long id, @NonNull String content,  String authorUsername, @NonNull Long postId, LocalDateTime comment_date) {
         this.id = id;
         this.content = content;
         this.authorUsername = authorUsername;
